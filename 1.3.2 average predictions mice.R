495a7b1cea44704761053cba479090acc1decd86
jenniferfletcher1 <77435827+jenniferfletcher1@users.noreply.github.com>
2021-09-23 16:46:31 +0100

    Add files via upload

---
+++ 1.3.2 average predictions mice.R
@@ -0,0 +1,21 @@
+##############################################################################################
+# generating dataframe of prediction measures & parameter estimates
+# with corresponding standard errors & 95% CI's
+##############################################################################################
+
+averageing_predictions_mice <- function(predictions_df){
+  
+  # take average of each numeric column
+  average_predictions_df <- t(as.data.frame(colMeans(predictions_df[sapply(predictions_df, is.numeric)])))
+  # add name of dataset
+  average_predictions_df <- cbind(average_predictions_df, predictions_df$name_dataset[1]) 
+  # update column names
+  colnames(average_predictions_df) <- colnames(predictions_df)
+  # remove row names
+  rownames(average_predictions_df) <- NULL
+  
+  # set to data frame
+  average_predictions_df <- as.data.frame(average_predictions_df)
+  average_predictions_df
+  
+}
