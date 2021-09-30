4b86c49fe0165b7a7301a272be46b7a7d6ff4e48
jenniferfletcher1 <77435827+jenniferfletcher1@users.noreply.github.com>
2021-09-30 20:41:39 +0100

    Update 1.2 Functions: apply missing data methods.R

--- 1.2 Functions: apply missing data methods.R
+++ 1.2 Functions: apply missing data methods.R
@@ -13,27 +13,30 @@ generating_CCA <- function(dataset){
 ##############################################
 # mean / median & mode imputation: 
 # mode: X1 is a categorical variable
-# mean / median: X4 is a categorical variable
+# mean: X4 is a continuous variable
 ##############################################
 
+dataset <- generated_dataset_MCAR
+
 mode_imputation <- function(dataset){
-  # introduce a new variable for X1 to impute
-  dataset$X1_mode_imp <- dataset$X1
-  # create function for mode
-  mode <- function(var){which.max(table(var))} # create function for mode
-  # mode for X1
-  mode_X1 <- mode(var = dataset$X1)
+  
+  # mode for observed X1
+  data_CCA_X1 <- filter(dataset, X1.I == 1)
+  mode_X1 <- mfv(data_CCA_X1$X1)
+  
   # impute "missing values" with mode
-  dataset$X1_mode_imp <- as.factor(ifelse(dataset$X1.I == 0, as.numeric(mode_X1), as.numeric(as.character(dataset$X1))))
+  dataset$X1_mode_imp <- as.factor(ifelse(dataset$X1.I == 0, as.numeric(as.character(mode_X1)), as.numeric(as.character(dataset$X1_miss))))
   dataset
 }
 
+
 mean_imputation <- function(dataset){
-  # introduce a new variable for X4 to impute
-  dataset$X4_mean_imp <- dataset$X4
-  # mean
-  mean_X4 <- mean(dataset$X4_mean_imp)
+
+  # mean for observed X4
+  data_CCA_X4 <- filter(dataset, X4.I == 1)
+  mean_X4 <- mean(data_CCA_X4$X4)
+  
   # impute "missing values" with mean
-  dataset$X4_mean_imp <- as.numeric(ifelse(dataset$X4.I == 0, as.numeric(mean_X4), as.numeric(dataset$X4_mean_imp)))
+  dataset$X4_mean_imp <- as.numeric(ifelse(dataset$X4.I == 0, as.numeric(mean_X4), as.numeric(dataset$X4_miss)))
   dataset
 }
